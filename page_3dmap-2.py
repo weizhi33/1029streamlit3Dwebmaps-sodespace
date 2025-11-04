1d23042f5e54499a0b352996127429eec367399a
weizhi33 <a37705040@gmail.com>
2025-11-04 16:45:03 +0000

    modified:   page_3dmap-2.py

--- page_3dmap-2.py
+++ page_3dmap-2.py
@@ -1,19 +1,19 @@
 import streamlit as st
 import plotly.express as px
 import pandas as pd
-st.title("Plotly 3D W (Uß - Q)")
+st.title("Plotly 3D ")
 # --- 1. / Plotly wöÏ ---
 df = px.data.gapminder().query("year == 2007")
-# --- 2.  3D vW (scatter_geo) ---
+# --- 2.  3D  (scatter_geo) ---
 fig = px.scatter_geo(
 df,
-locations="iso_alpha", # _ïü
+locations="iso_alpha", # _
 color="continent", # wN
-hover_name="country", # _]
+hover_name="country", # 
 size="pop", # ö//ï?
-# *** :o "orthographic" Ï 3D 
+# *** : "orthographic" 
 Q ***
 projection="orthographic"
 )
-# --- 3.  Streamlit o ---
+# --- 3. Streamlit o ---
 st.plotly_chart(fig, use_container_width=True)
